eaf3b419471ed7cc3eb74e60f21decefe61df9f2
iatenine <iatenine@gmail.com>
2022-10-31 23:05:36 -0500

    Allow users to select districts dynamically Also improvements to hooks

--- src/hooks/useFetchMembers.ts
+++ src/hooks/useFetchMembers.ts
@@ -1,26 +1,24 @@
 import { useEffect, useState } from "react";
 
-const url = process.env.SERVER_URL || "http://localhost:3001";
+const url = process.env.SERVER_URL || "https://octo-spork.herokuapp.com";
+
 export const useFetchMembers = () => {
   const [members, setmembers] = useState<any[]>([]);
   const [stateCode, setStateCode] = useState<string>("");
-  const [district, setDistrict] = useState<number>(0);
+  const [district, setDistrict] = useState<number | string>("");
 
   useEffect(() => {
     const queryServer = async () => {
-      const res = await fetch(`${url}/representatives?state=${stateCode}`);
+      const districtParam =
+        district !== "At-Large" && Number(district) !== 0
+          ? `district=${district}`
+          : ``;
+      const res = await fetch(
+        `${url}/representatives?state=${stateCode + districtParam}`
+      );
       const body: Array<{ title: string; district: string; at_large: any }> =
         await res.json();
-      const relevant = body.filter(
-        (member: { title: string; district: string; at_large: any }) => {
-          return (
-            "Representative".toLowerCase() !== member.title.toLowerCase() ||
-            parseInt(member.district) === district ||
-            member.at_large
-          );
-        }
-      );
-      setmembers(relevant);
+      setmembers(body);
     };
     queryServer();
   }, [stateCode, district]);
